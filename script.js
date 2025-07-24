937c3983a32cd11ddd4cafe1dcc479da0c478da0
Devendra Kumar Ojha <devojha0099@gmail.com>
2025-07-24 09:43:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+const table = document.querySelector("table")
 
 const getSum = () => {
 //Add your code here
+	const old = document.querySelector(".total-row");
+	if(old) old.remove();
+	const prices = document.querySelectorAll(".price");
+	let sum = 0;
+	prices.forEach(ele=>{
+		sum = sum + Number(ele.textContent);
+	})
+	
+	const tr = document.createElement("tr");
+	tr.classList.add("total-row");
+	const td = document.createElement("td");
+	td.textContent = `Total Price ${sum}`
+	td.colSpan = 2;
+	tr.appendChild(td);
+	table.appendChild(tr)
+
+	
   
 };
 
